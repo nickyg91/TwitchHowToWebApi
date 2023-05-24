b20658aa6cc4609551855097cb70e858009b3355
Nick Ganter <nickganter91@gmail.com>
2023-05-24 11:20:10 -0500

    Added a type for the event emitter subscriptions

--- Orchard.Web/orchard-ui/src/stores/modal.store.ts
+++ Orchard.Web/orchard-ui/src/stores/modal.store.ts
@@ -1,12 +1,12 @@
 import { defineStore } from 'pinia';
 import { reactive, type Component, markRaw } from 'vue';
-
+type EventEmitterType = { [key in string]: Function };
 export type Modal = {
   title?: string | null;
   opened: boolean;
   view: Component;
   actions?: ModalAction[] | null;
-  events: Object | undefined;
+  events: EventEmitterType | undefined;
 };
 export type ModalAction = {
   label: string;
@@ -19,14 +19,14 @@ export const useModal = defineStore('modalStore', () => {
     opened: false,
     view: {},
     actions: [],
-    events: Object
+    events: undefined
   });
 
   function open(
     view: Component,
     title?: string | null,
     actions?: ModalAction[] | null,
-    events?: Object
+    events?: EventEmitterType
   ) {
     modalState.title = title;
     modalState.opened = true;
