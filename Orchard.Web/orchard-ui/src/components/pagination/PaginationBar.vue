3e9a03ddba7e927a6ea5eb84898822c0bdac7971
Nick Ganter <nickganter91@gmail.com>
2023-06-01 07:32:00 -0500

    Started pagination page truncation.

--- Orchard.Web/orchard-ui/src/components/pagination/PaginationBar.vue
+++ Orchard.Web/orchard-ui/src/components/pagination/PaginationBar.vue
@@ -5,26 +5,35 @@ const props = defineProps<{
   currentPageNumber: number;
   totalPages: number;
 }>();
-const firstPage = 1;
 const lastPage = computed(() => props.totalPages);
 
 const pages = computed<number[]>(() => {
-  const middle = Math.floor(props.totalPages);
-  return [middle - 1, middle, middle + 1];
+  const isLessThanFivePages = props.totalPages < 5;
+  const pagesArr: number[] = [];
+  if (isLessThanFivePages) {
+    for (let i = 0; i < props.totalPages; i++) {
+      pagesArr.push(i + 1);
+    }
+  } else {
+    if (props.currentPageNumber >= 5) {
+      for (let i = props.currentPageNumber - 1; i > props.currentPageNumber - 6; i--) {
+        pagesArr.push(i + 1);
+      }
+    }
+
+    for (let i = props.currentPageNumber - 1; i < props.currentPageNumber + 6; i++) {
+      pagesArr.push(i + 1);
+    }
+  }
+  return pagesArr;
 });
 </script>
 <template>
-  <div class="p-1 is-flex is-justify-content-center">
+  <div class="p-1 is-centered">
     <div class="pagination">
+      <a class="pagination-previous">Previous</a>
+      <a class="pagination-next">Next page</a>
       <ul class="pagination-list">
-        <li>
-          <a :class="{ 'is-current': currentPageNumber === firstPage }" class="pagination-link">{{
-            firstPage
-          }}</a>
-        </li>
-        <li>
-          <span class="pagination-ellipsis">&hellip;</span>
-        </li>
         <li v-for="page in pages" :key="page">
           <a :class="{ 'is-current': currentPageNumber === page }" class="pagination-link">{{
             page
